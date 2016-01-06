8f01a0659229aca3343154ab7882f026766a9f37
JessicaHerford <jrherford12@gmail.com>
2016-01-06 09:29:44 -0700

    hipsta

--- son-that-never-ends/routes/index.js
+++ son-that-never-ends/routes/index.js
@@ -9,18 +9,21 @@ router.get('/', function(req, res, next) {
   //pass control to the next handler, ie /verse-2
 });
 
+//Get the next page
 router.get('/verse-2', function(req, res) {
   res.render('index', {title: 'Verse 2', header1: '/', header2:' | ', header3: 'https://www.youtube.com/watch?v=HNTxr2NJHa0',
   lyric: 'It just goes on and on my friends', pic: 'http://i.ytimg.com/vi/1_47KVJV8DU/mqdefault.jpg',
   next: '/verse-3'})
 })
 
+//Get the next page
 router.get('/verse-3', function(req, res) {
   res.render('index', {title: 'Verse 3', header1: '/', header2:' | ', header3: 'https://www.youtube.com/watch?v=HNTxr2NJHa0',
   lyric: 'Some people, started singing it not knowing what it was', pic: 'http://media-cache-ak0.pinimg.com/236x/4d/61/6c/4d616c5f67a37d1b7db7acc79a3dcf03.jpg',
   next: '/verse-4'})
 })
 
+//Get the next page
 router.get('/verse-4', function(req, res) {
   res.render('index', {title: 'Verse 4', header1: '/', header2:' | ', header3: 'https://www.youtube.com/watch?v=HNTxr2NJHa0',
   lyric: 'And they\'ll continue singing it forever just because...', pic: 'http://media-cache-ak0.pinimg.com/236x/75/a1/d4/75a1d4cba1954c1abee9b4bd85d21de9.jpg',
